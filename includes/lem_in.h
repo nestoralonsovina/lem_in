a92e173bf58c79681e8e6eb09c89233817872138
Néstor Alonso Viña <alonsovinanestor@gmail.com>
2019-05-22 14:23:38 +0200

    forgot change

--- includes/lem_in.h
+++ includes/lem_in.h
@@ -90,10 +90,7 @@ typedef struct	s_paths {
     t_edge			**path;
 	t_path			**move;
 	int				len;
-<<<<<<< HEAD
 	double			predicted_ants;
-=======
->>>>>>> jallen
     int				mf;
     int				mc;
     int				time;
